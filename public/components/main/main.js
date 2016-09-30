db610b83ee578f94057cf42d41adc7306eb65b39
Brian Wynn <b_wynn@apple.com>
2016-09-30 16:47:40 -0700

    Main equation methods and unit tests completed and passing.

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -1,7 +1,115 @@
 angular.module('MainController', [])
     .controller('mainController', ['$scope', function($scope) {
 
+        // set undefined $scope variables
+        $scope.x, $scope.y;
+
         // explicitly set primitive value to numeric value of 0
         $scope.baseValue = Number(0);
+
+        // set $scope.operation to be undefined
+        $scope.operation = undefined;
+
+        // SET X VALUE 
+
+        // SET Y VALUE
+
+        // CLEAR BASE VALUE
+
+        // SCOPE EQUATION METHODS ----------------------------------------------
+        //
+        // these methods expose two values,
+        // 1. the returned value of the operation a user selects.
+        // 2. setting the string value of the $scope.operation which will be
+        //    used when the $scope.equals method is called
+
+        // method for addition
+        $scope.add = function() {
+            // evaluate the equation
+            var evaluate = function() {
+                return $scope.baseValue = $scope.x + $scope.y;
+            };
+            // set the case for the operation
+            var operation = function() {
+                return $scope.operation = "add";
+            };
+
+            return {
+                evaluate : evaluate,
+                operation: operation
+            };
+        };
+
+        // method for subtraction
+        $scope.subtract = function() {
+            // evaluate equation
+            var evaluate = function() {
+                return $scope.baseValue = $scope.x - $scope.y;
+            };
+            // set the case for the operation
+            var operation = function() {
+                return $scope.operation = "subtract";
+            };
+
+            return {
+                evaluate: evaluate,
+                operation: operation
+            };
+        };
+
+        // method for multiplication
+        $scope.multiply = function() {
+            // evaluate equation
+            var evaluate = function() {
+                return $scope.baseValue = $scope.x * $scope.y;
+            };
+            // set case for operation
+            var operation = function() {
+                return $scope.operation = "multiply";
+            };
+
+            return {
+                evaluate: evaluate,
+                operation: operation
+            };
+        };
+
+        // method for division
+        $scope.divide = function() {
+            // evaluate equation
+            var evaluate = function() {
+                return $scope.baseValue = $scope.x / $scope.y;
+            };
+            // set case for operation
+            var operation = function() {
+                return $scope.operation = "divide";
+            };
+
+            return {
+                evaluate: evaluate,
+                operation: operation
+            };
+        };
+
+        // EQUALS EVALUATION SWITCH CASE ---------------------------------------
+        //
+        // uses a switch case to determine which function to use to evaluate
+        // the x and y scope variables.
+        $scope.equals = function() {
+            switch ($scope.operation) {
+                case "add":
+                    $scope.add().evaluate();
+                    break;
+                case "subtract":
+                    $scope.subtract().evaluate();
+                    break;
+                case "multiply":
+                    $scope.multiply().evaluate();
+                    break;
+                case "divide":
+                    $scope.divide().evaluate();
+                    break;
+            }
+        };
 
     }]);
--- public/components/main/main.spec.js
+++ public/components/main/main.spec.js
@@ -1,7 +1,5 @@
 describe('mainController', function() {
 
-    //beforeEach(module('ngCalcApp'));
-
     var $controller, mainController;
 
     beforeEach(angular.mock.module('ngRoute'));
@@ -15,14 +13,140 @@ describe('mainController', function() {
     it('should be defined', function() {
         var $scope = {};
         var controller = $controller('mainController', {$scope: $scope});
-    })
+    });
+
+    describe('$scope.x', function() {
+        it('should be undefined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.x).not.toBeDefined();
+        });
+    });
+
+    describe('$scope.y', function() {
+        it('should be undefined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.y).not.toBeDefined();
+        });
+    });
 
     describe('$scope.baseValue', function() {
-        it('is set to the value of 0 on initialization', function() {
+        it('value should be a number set to 0', function() {
             var $scope = {};
             var controller = $controller('mainController', {$scope: $scope});
             expect(typeof $scope.baseValue).toEqual('number');
             expect($scope.baseValue).toEqual(0);
         });
     });
+
+    describe('$scope.operation', function() {
+        it('should be undefined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.operation).not.toBeDefined();
+        });
+    });
+
+    describe('$scope.add()', function() {
+
+        it('should be defined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.add).toBeDefined();
+        })
+
+        it('should return evaluate method for addition', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.x = 2;
+            $scope.y = 4;
+            $scope.add().evaluate();
+            expect($scope.baseValue).toEqual(6);
+        });
+
+        it('should set $scope.operation to "add"', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.add().operation();
+            expect($scope.operation).toBe("add");
+        });
+    });
+
+    describe('$scope.subtract()', function() {
+
+        it('should be defined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.subtract).toBeDefined();
+        });
+
+        it('should return evaluate method for subtraction', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.x = 6;
+            $scope.y = 3;
+            $scope.subtract().evaluate();
+            expect($scope.baseValue).toEqual(3);
+
+        });
+
+        it('should set $scope.operation to "subtract"', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.subtract().operation();
+            expect($scope.operation).toBe("subtract");
+        });
+    });
+
+    describe('$scope.multiply()', function() {
+
+        it('should be defined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.multiply).toBeDefined();
+        });
+
+        it('should return evaluate method for multiplication', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.x = 5;
+            $scope.y = 3;
+            $scope.multiply().evaluate();
+            expect($scope.baseValue).toEqual(15);
+        });
+
+        it('should set $scope.operation to "multiply"', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.multiply().operation();
+            expect($scope.operation).toBe("multiply");
+        });
+    });
+
+    describe('$scope.divide()', function() {
+
+        it('should be defined', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            expect($scope.divide).toBeDefined();
+        });
+
+        it('should return evaluate method for division', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.x = 60;
+            $scope.y = 5;
+            $scope.divide().evaluate();
+            expect($scope.baseValue).toEqual(12);
+        });
+
+        it('should set $scope.operation to "divide"', function() {
+            var $scope = {};
+            var controller = $controller('mainController', {$scope: $scope});
+            $scope.divide().operation();
+            expect($scope.operation).toBe("divide");
+        });
+    });
+
 });
